9a8bcbb99acdf8733d73c909ac4ad5114196ac16
Liza <fildepiorsta@gmail.com>
2024-04-29 14:12:32 +0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,20 @@ import React, { useEffect, useState } from "react";
 
 import "./App.css";
 
+import { CounterStartVersion } from "./CounterStartVersion";
+import { CounterWithRedux } from "./CounterWithRedux";
+import { Route, Routes } from "react-router-dom";
+import { Layout } from "./layout";
 
 function App() {
   return (
     <div className="App">
-     
+      <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/CounterStartVersion" element={<CounterStartVersion />}/>
+        <Route path="/CounterWithRedux" element={<CounterWithRedux />}/>
+        </Route>
+      </Routes>
     </div>
   );
 }
